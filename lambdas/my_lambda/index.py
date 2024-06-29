01e7c7d6ddcba1eac10086ab7b25476599e6789b
xiaoma <xiaoma@komavideo.com>
2024-06-29 10:34:03 +0900

    完善Lambda函数，增加接受客户端输入的部分

--- lambdas/my_lambda/index.py
+++ lambdas/my_lambda/index.py
@@ -5,4 +5,14 @@ import requests
 
 def main(event, context):
 
-    return {"statusCode": 200, "body": json.dumps("Hello from Lambda!")}
+    client_body = event["body"]
+    if client_body:
+        print(client_body)
+    else:
+        print("No body")
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps("Hello from Lambda!"),
+        "headers": {"Content-Type": "text/plain"},
+    }
