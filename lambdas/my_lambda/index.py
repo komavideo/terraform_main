bd3cc9ad6af1d2d942717189b54be0b8aa295ad9
xiaoma <xiaoma@komavideo.com>
2024-07-05 15:34:44 +0900

    Lambda函数增加计算执行时间

--- lambdas/my_lambda/index.py
+++ lambdas/my_lambda/index.py
@@ -1,9 +1,10 @@
 import json
 import boto3
-import requests
+import time
 
 
 def main(event, context):
+    start_time = time.time()  # 获取开始时间
 
     result = "Hello from Lambda!"
 
@@ -15,8 +16,14 @@ def main(event, context):
     else:
         result = "No body({})".format(http_method)
 
+    end_time = time.time()  # 获取结束时间
+    execution_time = "(程序运行时间：%.2f 秒)" % (
+        end_time - start_time
+    )  # 计算程序运行时间
+    print(execution_time)
+
     return {
         "statusCode": 200,
-        "body": result,
+        "body": "{}\n\n{}".format(execution_time, result),
         "headers": {"Content-Type": "text/plain"},
     }
