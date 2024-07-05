486547e9fb14ba0a2b204da826ccf5d6c14a90ed
xiaoma <xiaoma@komavideo.com>
2024-07-05 14:27:00 +0900

    增加region输出

--- 99-outputs.tf
+++ 99-outputs.tf
@@ -9,3 +9,7 @@
 output "aws_api_gateway_url" {
   value = "${aws_api_gateway_deployment.main.invoke_url}${aws_api_gateway_resource.main.path}"
 }
+
+output "aws_region" {
+  value = var.aws_region
+}
